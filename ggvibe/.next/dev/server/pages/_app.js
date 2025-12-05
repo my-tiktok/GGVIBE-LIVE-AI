9578ff11c7696ed425eb813e4e95fa9b6634256f
my-tiktok <221878800+my-tiktok@users.noreply.github.com>
2025-12-05 13:01:43 +0000

    chore: ignore Next.js build output

--- ggvibe/.next/dev/server/pages/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__2b38f66d._.js")
-R.m("[project]/ggvibe/node_modules/next/app.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/ggvibe/node_modules/next/app.js [ssr] (ecmascript)").exports
--- ggvibe/.next/dev/server/pages/index.js
+++
@@ -1,8 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/index.js")
-R.c("server/chunks/ssr/61b73_next_dist_2080bd43._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__7d5f135e._.js")
-R.c("server/chunks/ssr/61b73_72dbbdbb._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__e6a4d965._.js")
-R.c("server/chunks/ssr/61b73_next_8870cfd5._.js")
-R.m("[project]/ggvibe/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/ggvibe/pages/index.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/ggvibe/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/ggvibe/node_modules/next/app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/ggvibe/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/ggvibe/pages/index.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/ggvibe/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/ggvibe/node_modules/next/app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
